cb737be14fcb3b09bf29c16f838526fe2ab89778
jreedie <reedie@galatea-associates.com>
2018-08-06 16:23:13 -0400

    deploy kubernetes cluster w/ terraform

--- vars/injectCreds.groovy
+++ vars/injectCreds.groovy
@@ -59,14 +59,7 @@ def call(init_token) {
 		rm dnsPrefix.JSON
 	""", returnStdout: true)
 
-	sh """
-		ls
-		echo sub: '$subID'
-		echo cID: '$clientID'
-		echo cS: '$clientSecret'
-		echo ten: '$tenantID'
-		echo dns: '$dnsPrefix'
-	"""
+
 
 	String terraVars = """azure_subcription_id = "$subID",
 	azure_client_id = "$clientID"
@@ -76,23 +69,15 @@ def call(init_token) {
 	ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwGVt2Zhiv+XvARIH7vuzOf5M1ztP0VkS07RIiGb2asM9f7nPj7tH0iy1i1nB5GII0+AMwMx90B4EhUXZsOWkmnZFzVWto2+6f5orz3Pl4axE9+7OeO0xOoyDiIrkDUVor02zzYY+dP8I5AGHLvG26gzz5tt5toWixtilJ4ZmXFkQwHB7ghOG1mhOrVxGbAqW44TC3IA1Ogh7zDycqsz2af2GDT5Sd0nFDciCJiH5ax7n9XtlY9zGfd6sfHFw3a62q5+mnTM4owGyWW7YTd3fQJ/2qR6XnPDKS/YfqtXfpX1q6os3i+LgoCZ2CRKyAD1xcooHQiTtu8+Y3HJeXWDDD james@james-VirtualBox"
 	"""
 
-
+	//populate secrets file -> apply terraform plan -> deploy cluster
 	writeFile file: "k8s.tfvars", text: "$terraVars"
-	sh "ls; cat k8s.tfvars"
-	// String output = sh(script: """
-	
-	// 	export VAULT_ADDR=$vault_addr
-	// 	vault login $secretToken > /dev/null
-	// 	vault kv get -field=id my-secret/data/subID
-	// 	vault kv get -field=id my-secret/data/clientID
-	// 	vault kv get -field=id my-secret/data/clientSecret
-	// 	vault kv get -field=id my-secret/data/tenantID  
-	// """, returnStdout: true)
+	sh """
+		terraform state rm ""
+		terraform init
+		terraform apply -auto-approve -var-file=k8s.tfvars
+	"""
+	azureCLI commands: [[exportVariablesString: '', script: 'az group deployment create --name k8s-cluster --resource-group kubegroup --template-file var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.json --parameters var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.parameters.json']], principalCredentialId: 'kubegroup_sp'
 
-	// sh """
-	// 	export VAULT_ADDR=$vault_addr
-	// 	echo $output
-	// """
 
 
 }
