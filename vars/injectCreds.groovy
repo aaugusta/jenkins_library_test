077e0cd118bd0994af9f7bc80906337398412a38
jreedie <reedie@galatea-associates.com>
2018-08-06 16:05:49 -0400

    inject creds into file

--- vars/injectCreds.groovy
+++ vars/injectCreds.groovy
@@ -76,14 +76,8 @@ def call(init_token) {
 	ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwGVt2Zhiv+XvARIH7vuzOf5M1ztP0VkS07RIiGb2asM9f7nPj7tH0iy1i1nB5GII0+AMwMx90B4EhUXZsOWkmnZFzVWto2+6f5orz3Pl4axE9+7OeO0xOoyDiIrkDUVor02zzYY+dP8I5AGHLvG26gzz5tt5toWixtilJ4ZmXFkQwHB7ghOG1mhOrVxGbAqW44TC3IA1Ogh7zDycqsz2af2GDT5Sd0nFDciCJiH5ax7n9XtlY9zGfd6sfHFw3a62q5+mnTM4owGyWW7YTd3fQJ/2qR6XnPDKS/YfqtXfpX1q6os3i+LgoCZ2CRKyAD1xcooHQiTtu8+Y3HJeXWDDD james@james-VirtualBox"
 	"""
 
-	sh """
-		rm secretID.JSON
-		rm secretToken.JSON
-		rm sub.JSON
-		rm tenant.JSON
-	"""
 
-	writeFile("k8s.tfvars", "$terraVars")
+	writeFile file: "k8s.tfvars", text: "$terraVars"
 	sh "ls; cat k8s.tfvars"
 	// String output = sh(script: """
 	
