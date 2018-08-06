963e720239b3233267541368dfa7cbb8ca65d4d8
jreedie <reedie@galatea-associates.com>
2018-08-06 14:45:20 -0400

    testing secret token extraction

--- vars/injectCreds.groovy
+++ vars/injectCreds.groovy
@@ -18,15 +18,14 @@ def call(init_token) {
 
 	//retrieves token to access secrets associated with given role
 	
-	String payload = /{"role_id": '$roleID', "secret_id": '$secretID'}/
+	String payload = /{"role_id": "'$roleID'", "secret_id": "'$secretID'"}/
 	sh(script: """
 		curl --request POST --data '$payload' '$vault_addr'/v1/auth/approle/login \
 		-o secretToken.JSON
 	""")
-	sh "ls"
 	sh "cat secretToken.JSON"
 	String secretToken = parseJSON("secretToken.JSON").auth.client_token
-	sh "cat secretToken.JSON; echo $secretToken"
+	sh "echo $secretToken"
 
 	// String output = sh(script: """
 	
