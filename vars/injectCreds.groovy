345d5ceec22f51bb472f3c26872f1e8e47dfac0c
jreedie <reedie@galatea-associates.com>
2018-08-14 14:20:55 -0400

    altered to reflect new vault scheme

--- vars/injectCreds.groovy
+++ vars/injectCreds.groovy
@@ -1,15 +1,50 @@
 import groovy.json.JsonSlurperClassic
 
 def call(init_token) {
+	/*
+		vaultToken	: passed in by user, used to retrieve secret ID
+		libToken: 	: used to retrieve Role ID of current project 
+		vault_addr 	: address of vault server for http api requests
+	*/
+
 	String vaultToken = init_token
-	String roleID = '20c5906d-6106-696e-9288-7e274df11f13'
+	String libToken = "a422adb9-abce-e295-e81b-3ed4e6408186"
 	String vault_addr = 'http://127.0.0.1:8200'
+
+
+	//looks up Token information so we can extract the policy
+	sh(script: """
+		curl --header "X-Vault-Token: $vaultToken" \
+			'$vault_addr'/v1/auth/token/lookup-self -o policy.json 
+	""", returnStdout: true)
+
+	def policies = parseJSON("policy.json").data.policies
+	String policy = "default"
+	for(int i = 0; i < policies.size(); i++) {
+		if(!policies[i].equals("default")) {
+			policy = policies[i]
+		}	
+	}
+	/*
+		policies for user tokens follow naming convention of ${projectName}-id,
+		so we remove the last 3 characters to retrieve the project name
+	*/
+	String project = policy.substring(0, policy.size()-3)
+
+	//retrieve Role ID using project name 
+	sh(script: """
+		curl --header "X-Vault-Token: $libToken" \
+			'$vault_addr'/v1/secret/roles/'$project' -o role.JSON
+	""", returnStdout: true)
+	String RoleID = parseJSON("role.JSON").data.roleID
+
+
 
 	//retrieves secret_id for approle authentication
 	sh(script: """
 		set +x
 		curl --header "X-Vault-Token: $vaultToken" \
-			 --request POST '$vault_addr'/v1/auth/approle/role/jenkins-azure/secret-id \
+			 --request POST '$vault_addr'/v1/auth/approle/role/'$project'-role/secret-id \
 			 -o secretID.JSON
 	""", returnStdout: true)
 	String secretID = parseJSON("secretID.JSON").data.secret_id
@@ -29,34 +64,25 @@ def call(init_token) {
 	sh(script: """
 		set +x
 		curl --header "X-Vault-Token: $secretToken" \
-		'$vault_addr'/v1/my-secret/data/subID -o subID.JSON
+		'$vault_addr'/v1/secret/'$project'/creds -o creds.JSON
 
-		curl --header "X-Vault-Token: $secretToken" \
-		'$vault_addr'/v1/my-secret/data/clientID -o clientID.JSON
 
-		curl --header "X-Vault-Token: $secretToken" \
-		'$vault_addr'/v1/my-secret/data/clientSecret -o clientSecret.JSON
-
-		curl --header "X-Vault-Token: $secretToken" \
-		'$vault_addr'/v1/my-secret/data/tenantID -o tenantID.JSON
-
-		curl --header "X-Vault-Token: $secretToken" \
-		'$vault_addr'/v1/my-secret/data/dnsPrefix -o dnsPrefix.JSON
 	""", returnStdout: true)
 	
-	String subID = parseJSON("subID.JSON").data.id
-	String clientID = parseJSON("clientID.JSON").data.id
-	String clientSecret = parseJSON("clientSecret.JSON").data.id
-	String tenantID = parseJSON("tenantID.JSON").data.id
-	String dnsPrefix = parseJSON("dnsPrefix.JSON").data.id
+	def creds = parseJSON("creds.JSON")
+	String subID = creds.data.subID
+	String clientID = creds.data.clientID
+	String clientSecret = creds.data.clientSecret
+	String tenantID = creds.data.tenantID
+
 
 	sh(script: """
 		set +x
-		rm subID.JSON
-		rm clientID.JSON
-		rm clientSecret.JSON
-		rm tenantID.JSON
-		rm dnsPrefix.JSON
+		rm creds.JSON
+		rm secretID.JSON
+		rm secretToken.JSON
+		rm role.JSON
+		rm policy.JSON
 	""", returnStdout: true)
 
 
@@ -65,19 +91,21 @@ def call(init_token) {
 	azure_client_id = "$clientID",
 	azure_client_secret = "$clientSecret",
 	azure_tenant_id = "$tenantID",
-	dns_prefix = "$dnsPrefix",
 	ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwGVt2Zhiv+XvARIH7vuzOf5M1ztP0VkS07RIiGb2asM9f7nPj7tH0iy1i1nB5GII0+AMwMx90B4EhUXZsOWkmnZFzVWto2+6f5orz3Pl4axE9+7OeO0xOoyDiIrkDUVor02zzYY+dP8I5AGHLvG26gzz5tt5toWixtilJ4ZmXFkQwHB7ghOG1mhOrVxGbAqW44TC3IA1Ogh7zDycqsz2af2GDT5Sd0nFDciCJiH5ax7n9XtlY9zGfd6sfHFw3a62q5+mnTM4owGyWW7YTd3fQJ/2qR6XnPDKS/YfqtXfpX1q6os3i+LgoCZ2CRKyAD1xcooHQiTtu8+Y3HJeXWDDD james@james-VirtualBox"
 	"""
 
 	//populate secrets file -> apply terraform plan -> deploy cluster
 	writeFile file: "k8s.tfvars", text: "$terraVars"
-	sh """
-		terraform state rm ""
-		terraform init
-		terraform apply -auto-approve -var-file=k8s.tfvars
-	"""
-	azureCLI commands: [[exportVariablesString: '', script: 'az group deployment create --name k8s-cluster --resource-group kubegroup --template-file var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.json --parameters var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.parameters.json']], principalCredentialId: 'kubegroup_sp'
-	sh "rm k8s.tfvars"
+
+	sh "cat k8s.tfvars"
+
+	// sh """
+	// 	terraform state rm ""
+	// 	terraform init
+	// 	terraform apply -auto-approve -var-file=k8s.tfvars
+	// """
+	// azureCLI commands: [[exportVariablesString: '', script: 'az group deployment create --name k8s-cluster --resource-group kubegroup --template-file var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.json --parameters var/lib/jenkins/workspace/pipeline_demo_master-YCLVMIFKQWOHG4NMQXMJVJZU3W6QMPWGKPDBHFPXCCLCPYAAV4UQ/_output/kubegroup-k8s-cluster/azuredeploy.parameters.json']], principalCredentialId: 'kubegroup_sp'
+	// sh "rm k8s.tfvars"
 
 
 
