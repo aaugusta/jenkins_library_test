f106eba7529baebb7f51e950d4c25d49aaa3abb2
jreedie <reedie@galatea-associates.com>
2018-08-14 14:48:27 -0400

    revert previous change

--- vars/injectCreds.groovy
+++ vars/injectCreds.groovy
@@ -33,8 +33,6 @@ def call(init_token) {
 
 	//retrieve Role ID using project name 
 	sh(script: """
-		cat '$project'
-
 		curl --header "X-Vault-Token: $libToken" \
 			'$vault_addr'/v1/secret/roles/'$project' -o role.JSON
 	""", returnStdout: true)
