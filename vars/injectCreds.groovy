0eb7864d2e2d5a3f5e3761bd8b5ac2428257b5fa
jreedie <reedie@galatea-associates.com>
2018-08-16 10:18:52 -0400

    updated injectCreds

---
+++ vars/injectCreds.groovy
@@ -0,0 +1,26 @@
+import com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider.FolderCredentialsProperty
+import com.cloudbees.hudson.plugins.folder.AbstractFolder
+import com.cloudbees.hudson.plugins.folder.Folder
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+
+
+def call(projectName, token){
+
+String id = "vault_token"
+Credentials c = new StringCredentialsImpl(CredentialsScope.GLOBAL, id, "description: Token for passing to library functions", "$token")
+
+
+Jenkins.instance.getAllItems(Folder.class)
+    .findAll{it.name.equals('$projectName')}
+    .each{
+        AbstractFolder<?> folderAbs = AbstractFolder.class.cast(it)
+        FolderCredentialsProperty property = folderAbs.getProperties().get(FolderCredentialsProperty.class)
+        if(property != null){
+            property.getStore().addCredentials(Domain.global(), c)
+            println property.getCredentials().toString()
+        }
+}
+}
